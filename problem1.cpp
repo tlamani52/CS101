6b67da06e118790170dfe047d42fb3b402f6af11
tlamani52 <143969118+tlamani52@users.noreply.github.com>
2025-07-03 21:01:45 -0400

    Update problem1.cpp
    fixed the indent

--- problem1.cpp
+++ problem1.cpp
@@ -2,19 +2,18 @@
 using namespace std;
 
 int main() {
-	    //Goal: Create a big X
-	        int n;
-		cin >> n;
-	          for (int r = 1; r <= n; r++) {
-	                    for (int c = 1; c <= n; c++){
-	                                if (r == c || r + c == n + 1) cout << "*";
-	                                            else cout << " ";
-	                                                    }
-	                                                            cout << endl;
-	                                                                }
-	                                                                    cout << endl;
-	    
-	                                                                       return 0;
+//Goal: Create a big X
+	int n;
+	cin >> n;
+	for (int r = 1; r <= n; r++) {
+	for (int c = 1; c <= n; c++){
+	if (r == c || r + c == n + 1) cout << "*";
+	else cout << " ";
+	}
+	cout << endl;
+	}
+	cout << endl;
+	return 0;
 	                                                                        }
 	                      
 	                    
